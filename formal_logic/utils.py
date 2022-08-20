b7e8dc8fc09c0f62d3dd3949ab17a7ba796a9813
honoka <hello@hello_world.com>
2022-08-20 13:21:54 +0900

    Add sampling functions to utils.py

--- formal_logic/utils.py
+++ formal_logic/utils.py
@@ -1,4 +1,6 @@
-from typing import Dict, Any
+from typing import Dict, Any, Tuple, List, Iterable
+import random
+
 from nltk.corpus import cmudict
 
 
@@ -18,3 +20,40 @@ def flatten_dict(dic: Dict[str, Any]) -> Dict[str, Any]:
             flat_dic[key] = val
     return flat_dic
 
+
+def weighted_samplings_wo_replacement(weights: List[Tuple[float, Any]]) -> Iterable[int]:
+    done_indexes = set()
+    for _ in range(0, len(weights)):
+
+        not_done_weights = []
+        shifted_idx_to_idx = {}
+        shifted_idx = 0
+        for idx, weight in enumerate(weights):
+            if idx in done_indexes:
+                continue
+
+            not_done_weights.append(weight)
+            shifted_idx_to_idx[shifted_idx] = idx
+            shifted_idx += 1
+            
+        if sum(not_done_weights) == 0.0:
+            return
+
+        sampled_shifted_idx = weighted_sampling(not_done_weights)
+        sampled_idx = shifted_idx_to_idx[sampled_shifted_idx]
+        yield sampled_idx
+        done_indexes.add(sampled_idx)
+
+
+def weighted_sampling(weights: List[float]) -> int:
+    weight_sum = sum(weights)
+    if weight_sum == 0.0:
+        raise ValueError()
+    normalized_weights = [weight / weight_sum for weight in weights]
+    r = random.random()
+    cum = min(1e-7, min(normalized_weights))  # start from positive value in order to ensure that cum reached 1.0 at the end of the for loop
+    for idx, weight in enumerate(normalized_weights):
+        cum += weight
+        if cum >= r:
+            return idx
+    raise Exception()
---
+++ tests/formal_logic/test_utils.py
@@ -0,0 +1,62 @@
+from typing import List
+from pprint import pprint
+from collections import defaultdict
+from formal_logic.utils import weighted_sampling, weighted_samplings_wo_replacement
+
+
+def test_weighted_sampling():
+    print('\n\n\n ======== test_weighted_sampling() ========')
+
+    def stochastic_test(weights: List[float], trial=10000):
+        print('\n\n') 
+        print(f'---- stochastic_test (weights={weights}) ----')
+
+        counts = defaultdict(int)
+        for _ in range(trial):
+            counts[weighted_sampling(weights)] += 1
+        pprint(dict(counts))
+
+        sum_weight = sum(weights)
+        ratios = [weight / sum_weight for weight in weights]
+        for sampled_idx, count in counts.items():
+            assert(abs(count - trial * ratios[sampled_idx]) < trial * 0.05)
+
+    stochastic_test([1.0, 0.0, 0.0])
+    stochastic_test([0.0, 1.0, 1.0])
+    stochastic_test([0.4, 0.3, 0.3])
+    stochastic_test([1.0, 1.0, 1.0])
+
+
+def test_weighted_samplings_wo_replacement():
+
+    print('\n\n\n ======== test_weighted_samplings_wo_replacement() ========')
+
+    def stochastic_test(weights: List[float], trial=10000):
+        print('\n\n') 
+        print(f'---- stochastic_test (weights={weights}) ----')
+
+        non_zero_weight_idxs = [i_weight for i_weight, weight in enumerate(weights) if weight != 0.0]
+        counts = defaultdict(int)
+        for _ in range(trial):
+            sampled_idxs = [sampled_idx for sampled_idx in weighted_samplings_wo_replacement(weights)]
+
+            assert(len(sampled_idxs) == len(non_zero_weight_idxs))
+            assert(set(sampled_idxs) == set(non_zero_weight_idxs))
+
+            counts[sampled_idxs[0]] += 1  # only the first sample is unbiased.
+        pprint(dict(counts))
+
+        sum_weight = sum(weights)
+        ratios = [weight / sum_weight for weight in weights]
+        for sampled_idx, count in counts.items():
+            assert(abs(count - trial * ratios[sampled_idx]) < trial * 0.05)
+
+    stochastic_test([1.0, 0.0, 0.0])
+    stochastic_test([0.0, 1.0, 1.0])
+    stochastic_test([0.4, 0.3, 0.3])
+    stochastic_test([1.0, 1.0, 1.0])
+
+
+if __name__ == '__main__':
+    test_weighted_sampling()
+    test_weighted_samplings_wo_replacement()
