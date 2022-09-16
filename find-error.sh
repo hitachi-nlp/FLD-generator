b5fc4bfe73cf9a03e2dcc9b48014c4bf2e7315ae
honoka <hello@hello_world.com>
2022-09-16 14:59:57 +0900

    Update ./find-error.sh

--- find-error.sh
+++ find-error.sh
@@ -9,5 +9,6 @@ fi
 find ${INPUT_DIR}/ -type f\
   | grep "log.*txt\|qsub\.err\|qsub\.out\|zlog\|\.log$"\
   | sort\
-  | ack -l --files-from=- 'Traceback|Kill|ERROR|Error|WARNING'\
-  | ack -L --files-from=- 'resource_tracker.py'
+  | ack -l --files-from=- 'Traceback|Kill|ERROR|Error'\
+  | ack -L --files-from=- 'resource_tracker.py'\
+  | ack -L --files-from=- '_extend_braches() failed'
