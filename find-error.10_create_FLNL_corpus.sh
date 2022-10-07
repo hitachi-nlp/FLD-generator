25e6fd7ff1db85bda7486136dc8a488407ab9cc2
honoka <hello@hello_world.com>
2022-10-07 18:55:38 +0900

    Add find-error script for 10.create_FLNL_corpus.sh

--- find-error.10_create_FLNL_corpus.sh
+++ find-error.10_create_FLNL_corpus.sh
@@ -6,7 +6,9 @@ if [ "${INPUT_DIR}" = "" ]; then
   echo "Specify input directory"
 fi
 
+find ${INPUT_DIR} -type f | sort | grep jsonl$ | grep -v "job-" | xargs wc -l
+
 find ${INPUT_DIR}/ -type f\
   | grep "log.*txt\|qsub\.err\|qsub\.out\|zlog\|\.log$"\
   | sort\
-  | ack -l --files-from=- 'Traceback|Kill|ERROR|Error|Exception'
+  | ack -l --files-from=- 'Traceback|Kill|ERROR|Exception'
