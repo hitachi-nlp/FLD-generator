c6a4e8dd8398d0fac6630e5b475e9e3dea33bbf0
honoka <hello@hello_world.com>
2022-11-27 14:07:50 +0900

    Run experiments ./outputs/10.create_FLNL_corpus/20221126.transl.conclude_hypothesis_from_random_sent_if_proof_is_unknown

--- launch_create_FLNL_corpus.py
+++ launch_create_FLNL_corpus.py
@@ -241,7 +241,8 @@ def main():
 
     # output_top_dir = Path('./outputs/10.create_FLNL_corpus/20221124.and.simplified_formula_in_various_formulas')
     # output_top_dir = Path('./outputs/10.create_FLNL_corpus/20221125.full')
-    output_top_dir = Path('./outputs/10.create_FLNL_corpus/20221126.transl')
+    # output_top_dir = Path('./outputs/10.create_FLNL_corpus/20221126.transl')
+    output_top_dir = Path('./outputs/10.create_FLNL_corpus/20221126.transl.conclude_hypothesis_from_random_sent_if_proof_is_unknown')
 
     dataset_names = [
         # '20221007.atmf-PA.arg-compl.dpth-3.add-axioms-theorems',
