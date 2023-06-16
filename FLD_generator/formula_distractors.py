d65d85b6abfef0b266123ff7ce178e3442be38da
honoka <hello@hello_world.com>
2023-06-16 15:50:23 +0900

    [speedup] remove distractor strength check because is is just less than 'multiple proofs checking' and it is slow

--- FLD_generator/formula_distractors.py
+++ FLD_generator/formula_distractors.py
@@ -127,7 +127,7 @@ def _new_distractor_formula_is_ok(new_distractor: Formula,
         return False
 
     if not is_consistent_formula_set_z3([new_distractor] + existing_distractors):
-        logger.warning('reject the distractor because adding it will make distractors inconsistent')
+        logger.warning('reject new distractor because adding it will make distractors inconsistent')
         for dist in [new_distractor] + existing_distractors:
             logger.info(dist)
         return False
@@ -142,7 +142,7 @@ def _new_distractor_formula_is_ok(new_distractor: Formula,
     original_tree_is_consistent = is_consistent_formula_set_z3(leaf_formulas_in_tree)
     if original_tree_is_consistent and\
             not is_consistent_formula_set_z3([new_distractor] + existing_distractors + leaf_formulas_in_tree):
-        logger.warning('reject the distractor because adding it will make leaf formulas and distractors inconsistent')
+        logger.warning('reject new distractor because adding it will make leaf formulas and distractors inconsistent')
         for dist in [new_distractor] + existing_distractors + leaf_formulas_in_tree:
             logger.info(dist)
         return False
@@ -154,13 +154,15 @@ def _new_distractor_formula_is_ok(new_distractor: Formula,
             return False
 
     if not allow_other_proofs:
-        for tree_formula in [node.formula for node in proof_tree.nodes]:
-            if is_stronger_z3(new_distractor, tree_formula) or is_equiv_z3(new_distractor, tree_formula):  # SLOW
-                logger.warning('reject new_distractor %s because it is stronger or equals to a leaf formula %s',
-                               new_distractor.rep,
-                               tree_formula.rep)
-                return False
+        # -- we think this strength check is just less then the "other proof check" below --
+        # for tree_formula in [node.formula for node in proof_tree.nodes]:
+        #     if is_stronger_z3(new_distractor, tree_formula) or is_equiv_z3(new_distractor, tree_formula):  # SLOW
+        #         logger.warning('reject new distractor %s because it is stronger or equals to a leaf formula %s',
+        #                        new_distractor.rep,
+        #                        tree_formula.rep)
+        #         return False
 
+        # -- other proof check --
         _have_other_proofs, droppable_formula = have_other_proofs(
             leaf_formulas_in_tree,
             existing_distractors + [new_distractor],
