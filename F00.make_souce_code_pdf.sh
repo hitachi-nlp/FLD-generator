1fbf557726efdab76b2d251023d6b931e07a1f8a
honoka <hello@hello_world.com>
2023-07-02 12:30:19 +0900

    [refactor] remove files

--- F00.make_souce_code_pdf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-OUTPUT_DIR=./F00.source_code_pdf
-
-find ./FLNL -type f | grep "\.py$" > ${OUTPUT_DIR}/source_code.txt
-enscript -p ${OUTPUT_DIR}/source_code.ps ${OUTPUT_DIR}/source_code.txt
-ps2pdf ${OUTPUT_DIR}/source_code.ps ${OUTPUT_DIR}/source_code.pdf
