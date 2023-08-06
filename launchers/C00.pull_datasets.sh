c002bb7d069b1242d1bdfdce93e032b8f19cc26f
honoka <hello@hello_world.com>
2023-08-07 07:26:03 +0900

    [update] dataset pull scripts

--- launchers/C00.pull_datasets.sh
+++ launchers/C00.pull_datasets.sh
@@ -16,7 +16,7 @@
 
 
 source_ABIC_dir=/groups/1/gca50126/honoka/work/projects/FLNL/outputs/10.create_FLD_corpus/20230801.case_study_finalize.fix/dataset_name=20230729.case_study_finalize.D3/trnsltn_adj_vrb_nn_rt=1-1-1/
-output_dir=./outputs/C00.pull_datasets.sh/20230801.case_study_finalize.fix/FLD.v2/FLD.3
+output_dir=./outputs/C00.pull_datasets.sh/20230801.case_study_finalize.fix/FLD.v2/FLD.v2
 mkdir -p ${output_dir} 1>/dev/null 2>&1
 for split in "train" "valid" "test"; do
   rsync -av es.abci.ai:${source_ABIC_dir}/${split}/${split}.jsonl ${output_dir}
@@ -24,7 +24,7 @@ done
 
 
 source_ABIC_dir=/groups/1/gca50126/honoka/work/projects/FLNL/outputs/10.create_FLD_corpus/20230801.case_study_finalize.fix/dataset_name=20230729.case_study_finalize.D8/trnsltn_adj_vrb_nn_rt=1-1-1/
-output_dir=./outputs/C00.pull_datasets.sh/20230801.case_study_finalize.fix/FLD.v2/FLD.4
+output_dir=./outputs/C00.pull_datasets.sh/20230801.case_study_finalize.fix/FLD.v2/FLD-star.v2
 mkdir -p ${output_dir} 1>/dev/null 2>&1
 for split in "train" "valid" "test"; do
   rsync -av es.abci.ai:${source_ABIC_dir}/${split}/${split}.jsonl ${output_dir}
