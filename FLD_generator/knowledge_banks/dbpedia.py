6427a177f148c12f0c50506d4aab01e24da86d3a
MorishT <magi.magilmagi@gmail.com>
2023-10-28 20:01:31 +0900

    [update] dbpedia works

---
+++ FLD_generator/knowledge_banks/dbpedia.py
@@ -0,0 +1,455 @@
+from typing import Optional, Iterable, List, Dict, Tuple
+from enum import Enum
+import logging
+import random
+
+from FLD_generator.word_banks.word_utils import WordUtil, POS
+from .base import KnowledgeBankBase
+from .statement import (
+    Statement,
+    StatementType,
+    DeclareStatement,
+    IfThenStatement,
+    SomeoneX,
+    SomeoneY,
+    SomethingX,
+    SomethingY,
+)
+from .utils import (
+    parse_as_pred,
+    parse_as_subj,
+    parse,
+)
+
+
+logger = logging.getLogger(__name__)
+
+_WORD_UTILS = WordUtil('eng')
+
+
+_TRANSLATIONS: Dict[str, List[Tuple[str, POS]]] = {
+    """
+    gawk '{print $3}' ./res/knowledge_banks/DBpedia500/train1.txt | sort | uniq -c | sort -n -r > ./res/knowledge_banks/DBpedia500/relations.train1.txt"
+    """
+
+    'team': [
+        ('belongs to', POS.VERB, False),
+    ],
+    'birthPlace': [
+        ('was born in', POS.PAST_PARTICLE, False),
+    ],
+    'genre': [
+        ('is', POS.NOUN, False),
+        ('is kind of', POS.NOUN, False),
+    ],
+    'recordLabel': [
+        ('belongs to', POS.VERB, False),
+    ],
+    'starring': [
+        ('starrs', POS.VERB, False),
+        ('starred in', POS.VERB, True),
+    ],
+    'country': [
+        ('is in', POS.ADJ, False),
+    ],
+    'producer': [
+        ('was created by', POS.PAST_PARTICLE, False),
+        ('created', POS.VERB, True),
+    ],
+    'associatedMusicalArtist': [
+        # relation is ambiguous
+    ],
+    'associatedBand': [
+        # relation is ambiguous
+    ],
+    'deathPlace': [
+        ('died in', POS.VERB, False),
+    ],
+    'type': [
+        ('is', POS.NOUN, False),
+        ('is kind of', POS.NOUN, False),
+    ],
+    'isPartOf': [
+        ('is part of', POS.NOUN, False),
+    ],
+    'occupation': [
+        ('work as', POS.VERB, False),
+        ('is', POS.NOUN, False),
+    ],
+    'writer': [
+        ('is wrote by', POS.PAST_PARTICLE, False),
+        ('wrote', POS.VERB, True),
+    ],
+    'timeZone': [
+        # relation is too maniac
+    ],
+    'hometown': [
+        ('was born in', POS.PAST_PARTICLE, False),
+    ],
+    'subsequentWork': [
+        ('is previous work before', POS.NOUN, False),
+        ('is next work after', POS.NOUN, True),
+    ],
+    'battle': [
+        ('fought in', POS.VERB, False),
+    ],
+    'location': [
+        ('is located in', POS.PAST_PARTICLE, False),
+    ],
+    'previousWork': [
+        ('is next work after', POS.NOUN, False),
+        ('is next work after', POS.NOUN, True),
+    ],
+    'almaMater': [
+        ('graduated from', POS.VERB, False),
+    ],
+    'format': [
+        # relation is too maniac
+    ],
+    'successor': [
+        ('is predecessor of', POS.NOUN, False),
+        ('is successor of', POS.NOUN, True),
+    ],
+    'position': [
+        ('work as', POS.VERB, False),
+    ],
+    'instrument': [
+        ('is', POS.NOUN, False),
+    ],
+    'artist': [
+        ('was created by', POS.PAST_PARTICLE, False),
+        ('created', POS.VERB, True),
+    ],
+    'formerTeam': [
+        ('previously worked in', POS.VERB, False),
+    ],
+    'award': [
+        ('was awarded', POS.PAST_PARTICLE, False),
+    ],
+    'managerClub': [
+        ('manages', POS.VERB, False),
+        ('is managed by', POS.PAST_PARTICLE, True),
+    ],
+    'recordedIn': [
+        ('was recorded in', POS.PAST_PARTICLE, False),
+    ],
+    'director': [
+        ('was directed by', POS.PAST_PARTICLE, False),
+        ('is director of', POS.NOUN, True),
+    ],
+    'neighboringMunicipality': [
+        ('is near to', POS.ADJ, False),
+        ('is near to', POS.ADJ, True),
+    ],
+    'predecessor': [
+        ('is successor of', POS.NOUN, False),
+        ('is predecessor of', POS.NOUN, True),
+    ],
+    'distributor': [
+        ('is distributed by', POS.PAST_PARTICLE, False),
+        ('distributted', POS.VERB, False),
+    ],
+    'sisterStation': [
+        ('is sister station of', POS.NOUN, False),
+        ('is sister station of', POS.NOUN, True),
+    ],
+    'language': [
+        ('is spoken in', POS.PAST_PARTICLE, True),
+    ],
+    'musicalArtist': [
+        ('was created by', POS.PAST_PARTICLE, False),
+        ('created', POS.VERB, True),
+    ],
+    'party': [
+        ('belongs to', POS.VERB, False),
+    ],
+    'musicalBand': [
+        ('was created by', POS.PAST_PARTICLE, False),
+        ('created', POS.VERB, True),
+    ],
+    'musicComposer': [
+        ('was created by', POS.PAST_PARTICLE, False),
+        ('created', POS.VERB, True),
+    ],
+    'nationality': [
+        ('was born in', POS.PAST_PARTICLE, False),
+    ],
+    'album': [
+        # ??
+    ],
+    'family': [
+        ('is', POS.NOUN, False),
+        ('is kind of', POS.NOUN, False),
+    ],
+    'order': [
+        ('is', POS.NOUN, False),
+        ('is kind of', POS.NOUN, False),
+    ],
+    'class': [
+        ('is', POS.NOUN, False),
+        ('is kind of', POS.NOUN, False),
+    ],
+    'city': [
+        ('is located in', POS.PAST_PARTICLE, False),
+    ],
+    'computingPlatform': [
+        ('can be played on', POS.PAST_PARTICLE, False),
+    ],
+    'residence': [
+        ('lived in', POS.VERB, False),
+    ],
+    'formerBandMember': [
+        ('played in', POS.VERB, True),
+    ],
+    'product': [
+        ('produces', POS.VERB, False),
+    ],
+    'commander': [
+        ('commanded in', POS.VERB, True),
+    ],
+    'region': [
+        ('lived in', POS.VERB, False),
+    ],
+    'religion': [
+        ('believes in', POS.VERB, False),
+    ],
+    'influencedBy': [
+        ('was influenced by', POS.PAST_PARTICLE, False),
+    ],
+    'race': [
+        ('raced in', POS.VERB, False),
+    ],
+    'bandMember': [
+        ('played in', POS.VERB, True),
+    ],
+    'cinematography': [
+        ('was created by', POS.PAST_PARTICLE, False),
+        ('created', POS.VERB, True),
+    ],
+    'state': [
+        ('is located in', POS.PAST_PARTICLE, False),
+    ],
+    'industry': [
+        ('operates in', POS.VERB, False),
+    ],
+    'guest': [
+        ('took guest role in', POS.VERB, True),
+    ],
+    'phylum': [
+        ('is', POS.NOUN, False),
+        ('is kind of', POS.NOUN, False),
+    ],
+    'spouse': [
+        ('got married to', POS.VERB, False),
+        ('got married to', POS.VERB, True),
+    ],
+    'routeJunction': [
+        ('is route to', POS.NOUN, False),
+    ],
+    'field': [
+        ('specializes in', POS.VERB, False),
+    ],
+    'militaryBranch': [
+        ('workied in', POS.VERB, False),
+    ],
+    'restingPlace': [
+        ('died in', POS.VERB, False),
+    ],
+    # 'ground': [
+    #     ('hoge', POS.VERB, False),
+    # ],
+    # 'league': [
+    #     ('hoge', POS.VERB, False),
+    # ],
+    # 'editing': [
+    #     ('hoge', POS.VERB, False),
+    # ],
+    # 'part': [
+    #     ('hoge', POS.VERB, False),
+    # ],
+    # 'influenced': [
+    #     ('hoge', POS.VERB, False),
+    # ],
+    # 'publisher': [
+    #     ('hoge', POS.VERB, False),
+    # ],
+    # 'populationPlace': [
+    #     ('hoge', POS.VERB, False),
+    # ],
+    # 'profession': [
+    #     ('hoge', POS.VERB, False),
+    # ],
+    # 'network': [
+    #     ('hoge', POS.VERB, False),
+    # ],
+    # 'company': [
+    #     ('hoge', POS.VERB, False),
+    # ],
+    # 'affiliation': [
+    #     ('hoge', POS.VERB, False),
+    # ],
+    # 'district': [
+    #     ('hoge', POS.VERB, False),
+    # ],
+    # 'owner': [
+    #     ('hoge', POS.VERB, False),
+    # ],
+    # 'series': [
+    #     ('hoge', POS.VERB, False),
+    # ],
+    # 'keyPerson': [
+    #     ('hoge', POS.VERB, False),
+    # ],
+    # 'parent': [
+    #     ('hoge', POS.VERB, False),
+    # ],
+    # 'related': [
+    #     ('hoge', POS.VERB, False),
+    # ],
+    # 'author': [
+    #     ('hoge', POS.VERB, False),
+    # ],
+    # 'governmentType': [
+    #     ('hoge', POS.VERB, False),
+    # ],
+    # 'headquarter': [
+    #     ('hoge', POS.VERB, False),
+    # ],
+    # 'broadcastArea': [
+    #     ('hoge', POS.VERB, False),
+    # ],
+    # 'president': [
+    #     ('hoge', POS.VERB, False),
+    # ],
+    # 'voice': [
+    #     ('hoge', POS.VERB, False),
+    # ],
+    # 'locationCountry': [
+    #     ('hoge', POS.VERB, False),
+    # ],
+    # 'leaderName': [
+    #     ('hoge', POS.VERB, False),
+    # ],
+    # 'creator': [
+    #     ('hoge', POS.VERB, False),
+    # ],
+    # 'locationCity': [
+    #     ('hoge', POS.VERB, False),
+    # ],
+    # 'composer': [
+    #     ('hoge', POS.VERB, False),
+    # ],
+    # 'knownFor': [
+    #     ('hoge', POS.VERB, False),
+    # ],
+    # 'assembly': [
+    #     ('hoge', POS.VERB, False),
+    # ],
+    # 'place': [
+    #     ('hoge', POS.VERB, False),
+    # ],
+    # 'primeMinister': [
+    #     ('hoge', POS.VERB, False),
+    # ],
+    # 'owningCompany': [
+    #     ('hoge', POS.VERB, False),
+    # ],
+    # 'literaryGenre': [
+    #     ('hoge', POS.VERB, False),
+    # ],
+}
+
+
+def _load_statements(path: str,
+                     max_statements: Optional[int] = None,
+                     shuffle=False) -> Iterable[Statement]:
+    logger.info('loading DBPedia statements from file "%s"', path)
+
+    if shuffle:
+        lines = open(path).readlines()
+        random.shuffle(lines)
+    else:
+        lines = open(path)
+
+    for i_line, line in enumerate(lines):
+        if max_statements is not None and i_line >= max_statements:
+            break
+        line = _normalize(line)
+
+        e0_str, e1_str, rel_str = line.rstrip('\n').split('\t')
+        if rel_str not in _TRANSLATIONS:
+            continue
+
+        negated = False
+        pred_translations = _TRANSLATIONS[rel_str]
+        if len(pred_translations) == 0:
+            continue
+
+        pred_translation, pred_required_pos, is_reverse = random.choice(pred_translations)
+
+        if is_reverse:
+            subj_str = e1_str
+            pred_str = e0_str
+        else:
+            subj_str = e0_str
+            pred_str = e1_str
+
+        # subj, subj_left_modif, subj_right_modif = parse(subj_str, 'rightmost')
+        # subj_pos = POS.NOUN
+        subj, subj_left_modif, subj_right_modif, subj_pos = parse_as_subj(subj_str)
+
+        pred_translation_with_modif = f'{pred_translation} {pred_str}' if pred_translation else pred_str
+        pred, pred_left_modif, pred_right_modif, pred_pos = parse_as_pred(pred_translation_with_modif, prioritize_pos=pred_required_pos)
+
+        try:
+            statement = DeclareStatement(
+                subj=subj,
+                subj_right_modif=subj_right_modif,
+                subj_left_modif=subj_left_modif,
+                subj_pos=subj_pos,
+
+                pred=pred,
+                pred_left_modif=pred_left_modif,
+                pred_right_modif=pred_right_modif,
+                pred_pos=pred_pos,
+                negated=negated,
+                relation=rel_str,
+            )
+        except Exception as e:
+            logger.warning('failed to parse line "%s"', line)
+            continue
+
+        yield statement
+
+
+def _normalize(line: str) -> str:
+    line = line.replace('_', ' ')
+    return line
+
+
+class DBpedia(KnowledgeBankBase):
+
+    def __init__(self,
+                 path: str,
+                 shuffle=False,  # note that shuffl=True loads huge dataset once at first, which is slow.
+                 max_statements: Optional[int] = None):
+        self._path = path
+        self._shuffle = shuffle
+        self._max_statements = max_statements
+
+        super().__init__()
+
+    def _load_statements(self, type_: StatementType) -> Iterable[Statement]:
+        for stmt in _load_statements(self._path, max_statements=self._max_statements, shuffle=self._shuffle):
+            if stmt.type != type_:
+                continue
+            yield stmt
+
+    @property
+    def _statement_types(self) -> List[StatementType]:
+        return [
+            StatementType.Fa,
+            # StatementType.nFa,
+            # StatementType.Fx_Gx,
+            # StatementType.Fx_nGx,
+        ]
---
+++ tests/FLD_generator/knowledge_banks/test_dbpedia.py
@@ -0,0 +1,45 @@
+from pprint import pprint
+import logging
+
+from FLD_generator.knowledge_banks.dbpedia import (
+    _load_statements,
+    DBpedia,
+)
+from FLD_generator.knowledge_banks.statement import DeclareStatement, IfThenStatement
+from logger_setup import setup as setup_logger
+
+from shared import sample_mappings
+
+_PATH = './res/knowledge_banks/DBpedia500/train1.txt'
+
+def test_load_statements():
+    for statement in _load_statements(_PATH, max_statements=100000):
+        print('')
+
+        if isinstance(statement, DeclareStatement):
+            pprint(statement)
+            print(f'type={str(statement.type)}')
+
+        elif isinstance(statement, IfThenStatement):
+            pprint(statement.if_statement)
+            pprint(statement.then_statement)
+            pprint(statement.relation)
+            print(f'type={str(statement.type)}')
+
+        else:
+            raise Exception()
+
+
+def test_bank():
+    bank = DBpedia(_PATH)
+
+    sample_mappings(bank, '{F}{a}', n_trial=1000)
+
+
+
+
+if __name__ == '__main__':
+    setup_logger(level=logging.INFO)
+
+    # test_load_statements()
+    test_bank()
